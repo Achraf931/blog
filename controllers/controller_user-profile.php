8594412eb552560a1694f199b342e3c45af43b48
Achraf <hamrouni.pro@outlook.fr>
2019-04-22 21:11:36 +0200

    mod

--- controllers/controller_user-profile.php
+++ controllers/controller_user-profile.php
@@ -7,5 +7,5 @@ if(!isset($_SESSION['user']) OR $_SESSION['user']['is_admin'] == 1){
 require_once('./models/model_user-profile.php');
 
 $message = updateUser(isset($_POST['update']));
-$user = recupInfo(($_SESSION['user']));
+$user = recupInfo();
 require_once('./views/user-profile.php');
